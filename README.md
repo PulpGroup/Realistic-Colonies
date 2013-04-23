c961a918defce82675a8672ec18216a2a4614ce0
wamilou <wamilou@gmail.com>
2013-04-23 09:53:50 +0200

    (added) Glowing watermelon (not fluid scaling)
    Signed-off-by: wamilou <wamilou@gmail.com>

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ See [here](https://github.com/wamilou/Realistic-Colonies) !
 - A new gamemode, you can play with your friend (10%)
 - A better hud for entities and npcs (0%)
 - New models for trees (100%)
+- Realistic watermelon (50%) (Animation not fluid at all)
 
 Please remember, this branch is there for tester only, this branch will be a most of time bugged !
--- lua/entities/watermelon/init.lua
+++ lua/entities/watermelon/init.lua
@@ -13,43 +13,66 @@
 		local ent = ents.Create( "watermelon" )
 		ent:SetPos( SpawnPos + Vector(0,0,15) )
 		ent:Spawn()
+		ent:DropMelon()
 		return ent
 	end
 
       
-    function ENT:Initialize()
-		
+    function ENT:Initialize()		
 		self:SetModel("models/props_junk/watermelon01.mdl")
 		self:PhysicsInit( SOLID_VPHYSICS ) // Make us work with physics,
 		self:SetMoveType( SOLID_VPHYSICS ) // after all, gmod is a physics
 		self:SetSolid( SOLID_VPHYSICS ) // Toolbox 
 		self:GetPhysicsObject():Wake()
 		self:GetPhysicsObject():SetMass( 1 ) 
+		self:GetPhysicsObject():EnableMotion(false)
+		self:SetModelScale(0,0)
 		self.lastmelon = math.Round(CurTime())
 		self.age=0
+		self.grow=0
     end
 
 	function ENT:Think()
 		if GetConVarNumber("rc_remove")==1 then 
 			self:Remove()
 		end
-		self.age = self.age + GetConVarNumber("rc_planttime")*GetConVarNumber("rc_speed")
-		if self.age > GetConVarNumber("rc_watermelon_time") and treesCount() <= GetConVarNumber("rc_tree_maxs") then
-			local melon = ents.Create("watermelon_plant_small")
-			undo.ReplaceEntity(self.Entity,melon)
-			melon:SetPos(self:GetPos()+Vector(0,0,-5))
-			melon:Spawn()
-			melon:SetOwner(self.Owner)
-			self:Remove()
-		end
+
+		if self.grow >= 100 then
+			self.age = self.age + GetConVarNumber("rc_planttime")*GetConVarNumber("rc_speed")
+			if self.age > GetConVarNumber("rc_watermelon_time") and treesCount() <= GetConVarNumber("rc_tree_maxs") then
+				local melon = ents.Create("watermelon_plant_small")
+				undo.ReplaceEntity(self.Entity,melon)
+				melon:SetPos(self:GetPos()+Vector(0,0,-5))
+				melon:Spawn()
+				melon:SetOwner(self.Owner)
+				self:Remove()
+			end			
 		
-		self:NextThink( CurTime() + GetConVarNumber("rc_planttime") )
-		return true
+			self:NextThink( CurTime() + GetConVarNumber("rc_planttime") )
+			return true
+		else
+			self.grow = self.grow+GetConVarNumber("rc_time")*10
+			self:SetModelScale( (self.grow/100) , 0 )
+			
+			if self.grow>=100 then
+				self:DropMelon()
+			end
+		
+			self:NextThink( CurTime() + GetConVarNumber("rc_time") )
+			return true
+		end
 	end
 	
      
-     function ENT:OnTakeDamage(dmg)
-     end
+    function ENT:OnTakeDamage(dmg)
+    end
+	
+	function ENT:DropMelon()
+		self.grow=100
+		self:SetModelScale(1,0)
+		self:GetPhysicsObject():EnableMotion(true)
+		self:GetPhysicsObject():Wake()
+	end
 	 
 	    
 	/*---------------------------------------------------------
